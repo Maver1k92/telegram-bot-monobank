076287d9bc991b53ce920cab0eed714ed7305ae1
Maver1k92 <70327932+Maver1k92@users.noreply.github.com>
2022-04-13 17:47:32 +0300

    Update TelegramBotApp.java

--- src/main/java/TelegramBotApp.java
+++ src/main/java/TelegramBotApp.java
@@ -1,2 +1,18 @@
+
+import com.mybot1.feature.currency.CurrencyService;
+import com.mybot1.feature.currency.MonoBankCurrencyService;
+import com.mybot1.feature.currency.dto.Currency;
+import com.mybot1.feature.currency.ui.PrettyPrintCurrencyService;
+
 public class TelegramBotApp {
+    public static void main(String[] args) {
+        CurrencyService currencyService =  new MonoBankCurrencyService();
+
+        Currency usd = Currency.USD;
+        double rate = currencyService.getRate(usd);
+
+       String prettyText = new PrettyPrintCurrencyService().convert(rate, usd);
+
+        System.out.println(prettyText);
+    }
 }
